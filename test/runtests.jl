e84ad89ca400ba4effddb64b362478ce5197883c
xinkai-zhou <xinkaizhou@REDLBISCOR01.ad.medctr.ucla.edu>
2020-04-05 19:02:39 -0700

    edited sim

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,10 @@ module PkgTest
 # include("simulation.jl")
 # include("form-matrices-test.jl")
 # include("preliminary-test.jl")
-include("test2.jl")
+#include("test2.jl")
 # include("test3_benchmark.jl")
+
+include("sim-exp2.jl")
+
 
 end
--- test/sim-exp2.jl
+++ test/sim-exp2.jl
@@ -6,22 +6,22 @@ using MixedModelsBLB, MixedModels, Random, Distributions, DataFrames, CSV, RCall
 Random.seed!(1)
 # ((Int64(1e4), 20), (Int64(1e4), 50), (Int64(1e4), 20), (Int64(1e4), 50), (Int64(1e5), 20), (Int64(1e5), 50))
 datasizes = ((Int64(1e4), 20), (Int64(1e4), 50)) 
-for (N, reps) in datasizes
-    # simulate data
-    x1 = rand(Normal(0, 1), reps * N)
-    x2 = rand(Normal(0, 3), reps * N)
-    rand_slope = zeros(reps * N)
-    @views for j in 1:N
-        rand_slope[(reps * (j-1) + 1) : reps * j] = x1[(reps * (j-1) + 1) : reps * j] .* rand(Normal(0, 2), 1)
-    end
-    y = 1 .+ x1 + x2 + # fixed effects
-        repeat(rand(Normal(0, 1), N), inner = reps) + # random intercept, standard normal
-        rand_slope +
-        rand(Normal(0, 1), reps * N) # error, standard normal
-    id = repeat(1:N, inner = reps)
-    dat = DataFrame(y=y, x1=x1, x2=x2, id=id)
-    CSV.write(string("../data/exp2-N-", N, "-rep-", reps, ".csv"), dat)
-end
+#for (N, reps) in datasizes
+#    # simulate data
+#    x1 = rand(Normal(0, 1), reps * N)
+#    x2 = rand(Normal(0, 3), reps * N)
+#    rand_slope = zeros(reps * N)
+#    @views for j in 1:N
+#        rand_slope[(reps * (j-1) + 1) : reps * j] = x1[(reps * (j-1) + 1) : reps * j] .* rand(Normal(0, 2), 1)
+#    end
+#    y = 1 .+ x1 + x2 + # fixed effects
+#        repeat(rand(Normal(0, 1), N), inner = reps) + # random intercept, standard normal
+#        rand_slope +
+#        rand(Normal(0, 1), reps * N) # error, standard normal
+#    id = repeat(1:N, inner = reps)
+#    dat = DataFrame(y=y, x1=x1, x2=x2, id=id)
+#    CSV.write(string("../data/exp2-N-", N, "-rep-", reps, ".csv"), dat)
+#end
 
 
 # MixedModelsBLB
@@ -34,14 +34,15 @@ for (N, reps) in datasizes
         id_name = "id", 
         cat_names = [], 
         subset_size = N^0.6,
-        n_subsets = 2, 
-        n_boots = 3,
+        n_subsets = 10, 
+        n_boots = 2000,
         MoM_init = false,
         solver = Ipopt.IpoptSolver(),
         verbose = true
     )
     push!(blb_runtime, (time_ns() - time0)/1e9)
 end
+CSV.write("data/blb_runtime.csv", blb_runtime)
 
 # MixedModels + bootstrap
 B = 2000 # number of bootstrap samples
@@ -57,7 +58,7 @@ for (N, reps) in datasizes
     # Get CI
     push!(mixedmodels_runtime, (time_ns() - time0)/1e9)
 end
-
+CSV.write("data/mixedmodels_runtime.csv", mixedmodels_runtime)
 
 # Rcall, lme4 + bootstrap
 lme4_runtime = Vector{Float64}()
@@ -82,7 +83,7 @@ for (N, reps) in datasizes
     push!(lme4_runtime, (time_ns() - time0)/1e9)
 end
 
-
+CSV.write("data/lme4_runtime.csv", lme4_runtime)
 
 
 
